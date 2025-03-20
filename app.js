e534dee4a06a854c09c7ff469ac097be2a2a382a
phuongias <phng2912@gmail.com>
2025-03-20 13:41:04 +0100

    second try

--- app.js
+++ app.js
@@ -253,7 +253,7 @@ const createScene = async function () {
     //Virtuelle Welt laden -> Hill Valley
     engine.displayLoadingUI(); //Display the loading screen as the scene takes a few seconds to load
     const virtualWorldResult = await BABYLON.SceneLoader.ImportMeshAsync("", "./",
-        "firsttry.glb", scene);
+        "try2.glb", scene);
     engine.hideLoadingUI(); //Hide Loadingscreen once the scene is loaded
     for (let child of virtualWorldResult.meshes) {
         child.renderingGroupId = 1;
